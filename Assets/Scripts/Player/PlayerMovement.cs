f0e7e830661d9035ad8bb71c0ce582fd630055b3
Largegini <dkswjddus0215@gmail.com>
2026-01-28 11:42:37 +0900

    Player 이동, 회전보간 구현
    좌표기준 이동 구현하는 중

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,8 +6,13 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private int _moveSpeed;
     private Vector3 dir;
-
+    private float _rotateValue;
     private Animator _animator;
+
+    private void Awake()
+    {
+     Init();   
+    }
 
     private void Update()
     {
@@ -15,18 +20,37 @@ public class PlayerMovement : MonoBehaviour
         Move();
     }
 
+    private void Init()
+    {
+        _rotateValue = 0;
+    }
+
     private void Move()
     {
         dir.z = Input.GetAxisRaw("Vertical");
         dir.x = Input.GetAxisRaw("Horizontal");
-        
-        transform.Translate(transform.forward * _moveSpeed * dir.z * Time.deltaTime);
-        transform.Translate(transform.right * _moveSpeed * -(dir.x) * Time.deltaTime);
+
+        //transform.Translate(Vector3.forward * _moveSpeed * dir.z * Time.deltaTime);
+        if (Input.GetKey(KeyCode.A) ||
+            Input.GetKey(KeyCode.D) ||
+            Input.GetKey(KeyCode.W) ||
+            Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.forward * _moveSpeed * Time.deltaTime);
+        }
     }
 
     private void Rotate()
     {
-        transform.rotation = Quaternion.LookRotation(dir);
+
+        _rotateValue += dir.x;
+        //transform.rotation = Quaternion.LookRotation(dir);
+
+        //transform.rotation = Quaternion.Slerp(transform.rotation,
+        //    Quaternion.Euler(0, _rotateValue, 0), 0.5f);
+        transform.rotation = Quaternion.Slerp(transform.rotation,
+            Quaternion.LookRotation(dir), 0.01f);
+
         
         //transform.Rotate(Vector3.up, 100f*dir.x * Time.deltaTime);
     }
