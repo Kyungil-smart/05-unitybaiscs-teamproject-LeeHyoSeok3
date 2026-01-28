cbe371eca6e60e4959df97f291459df2e5517438
Jaehoon Jeong <wogns4168@naver.com>
2026-01-28 16:50:14 +0900

    박스 이펙트, 점수 추가 부분 테스트 구현

--- Assets/Scripts/Block/BlockView.cs
+++ Assets/Scripts/Block/BlockView.cs
@@ -1,16 +1,35 @@
-﻿using System;
+﻿using CartoonFX;
+using System;
 using UnityEngine;
 
 public class BlockView : MonoBehaviour, IPoolable
 {
+    [SerializeField] private CFXR_Effect _destroyEffectPrefab;
+    [SerializeField] private ScorePopup scorePopup;
     public void OnSpawn()
     {
         gameObject.SetActive(true);   
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            OnDespawn();
+        }
+    }
+
     public void OnDespawn()
     {
+        if (_destroyEffectPrefab != null)
+        {
+            Instantiate(_destroyEffectPrefab, transform.position, Quaternion.identity);
+        }
         gameObject.SetActive(false);
+        if (scorePopup != null)
+        {
+            scorePopup.Show(100, transform);
+        }
     }
 
     public void SetWorldPostion(Vector3 pos)
