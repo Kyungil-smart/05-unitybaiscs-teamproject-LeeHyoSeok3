c1107fe4031c31c091cd2a7f969820da5542c2b2
강세환(플밍4기) <ghks5658@gmail.com>
2026-01-30 19:01:34 +0900

    플레이어 속도 감소 공격
    플레이어를 참조하여 메서드 호출시 속도를 느리게 한다
    몬스터 공격 컴포넌트에서 지속시간, 감속 속도, 감속 보간치를 설정해야 한다

---
+++ Assets/Scripts/Monster/MonsterAttack.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterAttack : MonoBehaviour
+{
+    GameObject player;
+
+    [SerializeField] float _decelerationValue; // 속도 감소치
+    [SerializeField] float _duration; // 감속 지속시간
+    [SerializeField] private float _decelerationPerTime; // 감속 보간치
+
+
+
+
+    void OnEnable()
+    {
+        player = GameObject.Find("Player");
+    }
+    
+    void SlowPlayer()
+    {
+        // 플래이어 상태를 변화시키는 매서드
+        player.GetComponent<PlayerMovement>().Slow(_decelerationValue , _duration, _decelerationPerTime);
+        // 퇴장
+        gameObject.GetComponent<MonsterController>().Release();
+    }
+}
