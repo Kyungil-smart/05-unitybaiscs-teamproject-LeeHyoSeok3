ed8291952feb4bab648865950153a23857fd43de
강세환(플밍4기) <ghks5658@gmail.com>
2026-01-31 17:54:30 +0900

    정찰 몬스터 이동 로직

--- Assets/Scripts/Monster/Patrol_Monster.cs
+++ Assets/Scripts/Monster/Patrol_Monster.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class Patrol_Monster : MonoBehaviour
 {
-    [SerializeField][Range(1,20)] float _moveSpeed;
-    GridTile _currentTile;
-    GridTile _nextTile;
+    [SerializeField][Range(0,20)] float _moveSpeed;
+    
+    Rigidbody rb;
     Coroutine _movecoroutine;
-    LayerMask _gridLayer;
+    [SerializeField] LayerMask _blockLayer;
 
-    void Start()
+    void Awake()
     {
-        _gridLayer = LayerMask.NameToLayer("Grid");
+        rb = GetComponent<Rigidbody>();
     }
 
     
@@ -22,9 +22,10 @@ public class Patrol_Monster : MonoBehaviour
     {
         if(collision.gameObject.CompareTag("Block"))
         {
-            if(collision.transform.position.y > transform.position.y)
+            if(collision.transform.position.y > transform.position.y + 2f)
             {
                 Explosion();
+                gameObject.SetActive(false);
             }
         }      
     }
@@ -36,91 +37,90 @@ public class Patrol_Monster : MonoBehaviour
         // 이펙트 출력
         
     }
-
-    
-
-    IEnumerator Patrol()
+    IEnumerator WanderRoutine()
     {
-        if(_currentTile._upBlock && _currentTile._rightBlock && _currentTile._downBlock && _currentTile._leftBlock)
-        yield break; // 모든 방향이 막혀있으면 정지
-        if(_nextTile._blockOn)
-        PathFind(); // 가려던 방향이 막혀있으면 다음 타일 탐색
-
-        while(Vector3.Distance(transform.position, _nextTile.transform.position) <= 0.05f)
+        while (true)
         {
-            Rotate();
-            Move();
-            yield return null;
+            // 정지 및 대기
+            rb.velocity = Vector3.zero;
+            yield return YieldContainer.WaitForSeconds(1f);
+            
+            // 목표 방향 
+            
+            Vector3 targetDir = GetRandomDirection();
+            Quaternion targetRotation = Quaternion.LookRotation(targetDir);
+
+            
+            // 회전 루프
+            while (Quaternion.Angle(transform.rotation, targetRotation) > 1f)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 10f);
+                yield return null;
+            }
+
+            
+
+            
+            // 일정 시간 혹은 블럭 감지할때까지 이동
+            float moveTime = Random.Range(1f, 4f);
+            float timer = 0f;
+            
+            while (timer < moveTime)
+            {
+                if (IsObstacleInFront()) break;
+
+                rb.velocity = new Vector3(transform.forward.x * _moveSpeed, rb.velocity.y, transform.forward.z * _moveSpeed);
+
+                timer += Time.deltaTime;
+                yield return null;
+            }
+            
         }
-        PathFind();
-        _movecoroutine = StartCoroutine(Patrol());
+    }
+
+    Vector3 GetRandomDirection()
+    {
+        int rand = Random.Range(0, 4);
+        switch (rand)
+        {
+            case 0: 
+                return transform.forward; 
+            case 1: 
+                return -transform.forward; 
+            case 2: 
+                return transform.right; 
+            case 3: 
+                return -transform.right;
+        }
+        return transform.forward; 
         
     }
 
-    void PathFind()
+    bool IsObstacleInFront()
     {
-        int[] dir = new int[]{0,1,2,3};
-        Shuffle.Array(dir);
-        for(int i = 0; i < 4; i++)
-        {
-            switch(dir[i])
-            {
-                case 0 :
-                    if(!_currentTile._upBlock._blockOn)
-                    _nextTile = _currentTile._upBlock;
-                    break;
-                case 1:
-                    if(!_currentTile._rightBlock._blockOn)
-                    _nextTile = _currentTile._upBlock;
-                    break;
-                case 2:
-                    if(!_currentTile._downBlock._blockOn)
-                    _nextTile = _currentTile._upBlock;
-                    break;
-                case 3:
-                    if(!_currentTile._leftBlock._blockOn)
-                    _nextTile = _currentTile._upBlock;
-                    break;
-            }
-        }
+        // 레이캐스트로 앞쪽 장애물 감지
+        return Physics.Raycast(transform.position + Vector3.up * 1.2f, transform.forward, 0.5f, _blockLayer);
     }
 
-    void Move()
-    {
-        transform.Translate(Vector3.forward * Time.deltaTime * _moveSpeed);
-    }
-    void Rotate()  // 다음 위치를 바라보는 회전 로직
-    {
-        Quaternion targetTile = Quaternion.LookRotation(_nextTile.transform.position, transform.up);
-        transform.rotation = Quaternion.Slerp(transform.rotation, targetTile, 0.2f);
-    }
 
     void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.layer == _gridLayer)
-        {
-            _currentTile = other.GetComponent<GridTile>();
-        }
+        
     }
     void OnEnable()
     {
-        PathFind();
-        _movecoroutine = StartCoroutine(Patrol());
-        GameEventBus.Subscribe<GridUpdateEvent>(NullAct);
-
+        _movecoroutine = StartCoroutine(WanderRoutine());
     }
     void OnDisable()
     {
-        _currentTile = null;
-        _nextTile = null;
+        if(_movecoroutine != null) StopCoroutine(_movecoroutine);
         _movecoroutine = null;
-        GameEventBus.Unsubscribe<GridUpdateEvent>(NullAct);
 
     }
-    void NullAct(GridUpdateEvent evt)
+    void OnDrawGizmos()
     {
-        _movecoroutine = StartCoroutine(Patrol());
+        Vector3 startPos = transform.position + Vector3.up * 1.2f;
+        Vector3 rayDirection = transform.forward * 0.5f;
+        Gizmos.DrawRay(startPos, rayDirection);
     }
-
-    
 }
